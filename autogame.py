549f7f9f70f4fca593b8ddfe9b853b5b59afb41e
KhongPhaiNhen <81064900+spider-in-bathroom@users.noreply.github.com>
2023-09-20 19:12:13 +0700

    Add files via upload

--- autogame.py
+++ autogame.py
@@ -1,36 +1,40 @@
+# http://www.kbdedit.com/manual/low_level_vk_list.html
+
 import time as t 
 import win32gui, win32ui, win32con
+
 game = True
+loops = 0
 
 
 def main(loops):
-    t.time()
-    window_name = 'Leaf Blower Revolution'
-    hwnd = win32gui.FindWindow(None, window_name)
-    win = win32ui.CreateWindowFromHandle(hwnd)
-   
-    win.SendMessage(win32con.WM_KEYDOWN, 0x27, 0)
-    t.sleep(5)
-    win.SendMessage(win32con.WM_KEYUP, 0x27, 0)
-   
-    win.SendMessage(win32con.WM_KEYDOWN,0x28 ,0)
-    t.sleep(0.1)
-   
-    win.SendMessage(win32con.WM_KEYUP,0x28, 0)
-    win.SendMessage(win32con.WM_KEYDOWN, 0x25, 0)
-    t.sleep(5)
-    win.SendMessage(win32con.WM_KEYUP, 0x25, 0)
-   
-    win.SendMessage(win32con.WM_KEYDOWN, 0x26, 0)
-    t.sleep(0.1)
-    win.SendMessage(win32con. WM_KEYUP, 0x26, 0)
-   
-    passed = t.ctime()
+    while loops >= 0:
+        t.time()
+        window_name = 'Leaf Blower Revolution'
+        hwnd = win32gui.FindWindow(None, window_name)
+        win = win32ui.CreateWindowFromHandle(hwnd)
     
-    loops = loops + 1
-    print(f'loop {loops} finished at {passed}')
+        win.SendMessage(win32con.WM_KEYDOWN, 0x27, 0)
+        t.sleep(5)
+        win.SendMessage(win32con.WM_KEYUP, 0x27, 0)
+    
+        win.SendMessage(win32con.WM_KEYDOWN,0x28 ,0)
+        t.sleep(0.1)
+    
+        win.SendMessage(win32con.WM_KEYUP,0x28, 0)
+        win.SendMessage(win32con.WM_KEYDOWN, 0x25, 0)
+        t.sleep(5)
+        win.SendMessage(win32con.WM_KEYUP, 0x25, 0)
+    
+        win.SendMessage(win32con.WM_KEYDOWN, 0x26, 0)
+        t.sleep(0.1)
+        win.SendMessage(win32con. WM_KEYUP, 0x26, 0)
+    
+        passed = t.ctime()
+        
+        loops = loops + 1
+        print(f'loop {loops} finished at {passed}')
 
 
 while game == True:
-    loops = 0
-    main(loops) 
+    main(loops)
